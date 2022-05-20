f380769ddf4000619a2bfb122851913d68ce9271
Jitendra Sharma <Jitendrasharma@hcl.com>
2022-05-20 06:50:13 +0000

    Add project files.

---
+++ git-repo-experience/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace git_repo_experience
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ git-repo-experience/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="git_repo_experience.MvcApplication" Language="C#" %>
